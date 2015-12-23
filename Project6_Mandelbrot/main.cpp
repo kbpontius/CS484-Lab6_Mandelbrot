9a7765781343152e9510abf0b4683fc666d39128
Kyle Pontius <kbpontius@gmail.com>
2015-12-22 21:04:45 -0400

    Initial Commit

---
+++ Project6_Mandelbrot/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project6_Mandelbrot
+//
+//  Created by Kyle on 12/22/15.
+//  Copyright © 2015 Kyle Pontius. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
